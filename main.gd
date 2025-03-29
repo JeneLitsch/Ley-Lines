fd3ab364c49432c1f3a554a7d21d4bb6e39f06ee
Sera K. Litsch <litsch.jene@gmail.com>
2025-03-29 12:01:27 +0100

    Improved UI

--- main.gd
+++ main.gd
@@ -9,18 +9,11 @@ const CompLogicOr = preload("res://components/logic_or.gd");
 
 
 var _edit_mode : bool = false;
-var _selected_component = CompConduit;
+var _selected_component : Script = null;
 var _place_rotation : int = 0;
 var _place_position : Vector2i = Vector2i.ZERO;
 
 
-func _ready() -> void:
-	%Circuit.place_component(CompSource.new(Vector2i(0,0), 1));
-	%Circuit.place_component(CompConduit.new(Vector2i(1,0), 1));
-	%Circuit.place_component(CompConduit.new(Vector2i(2,0), 1));
-	%Circuit.place_component(CompConduit.new(Vector2i(3,0), 1));
-	%Circuit.place_component(CompConduit.new(Vector2i(4,0), 1));
-	%Circuit.place_component(CompSink.new(Vector2i(5,0), 1));
 
 
 
@@ -30,25 +23,28 @@ func _update_cursor():
 
 
 
+func _process(delta: float) -> void:
+	%Selector.visible = Input.is_action_pressed("open_selector");
+
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var mouse_position = get_global_mouse_position();
 		var coords = %Grid.local_to_map(mouse_position);
-		print(coords);
 		_place_position = coords;
 		_update_cursor();
+		
 	if event.is_action_pressed("primary_click"):
-		if _edit_mode:
+		if _selected_component and not %Selector.visible:
 			%Circuit.place_component(_selected_component.new(_place_position, _place_rotation));
 		else:
 			%Circuit.click_at(_place_position);
+			
 	if event.is_action_pressed("secondary_click"):
 		var mouse_position = get_global_mouse_position();
 		var coords = %Grid.local_to_map(mouse_position);
 		%Circuit.remove_component_at(coords);
-		
-	if event.is_action_pressed("change_mode"):
-		_toggle_edit_mode();
 	
 	if event.is_action_pressed("rotate_l"):
 		_place_rotation = (_place_rotation + 3) % 4;
@@ -57,32 +53,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("rotate_r"):
 		_place_rotation = (_place_rotation + 1) % 4;
 		_update_cursor();
-	
-	if event.is_action_pressed("debug_1"):
-		_selected_component = CompConduit;
-		print(_selected_component);
-	
-	if event.is_action_pressed("debug_2"):
-		_selected_component = CompSource;
-	
-	if event.is_action_pressed("debug_3"):
-		_selected_component = CompSink;
-
-	if event.is_action_pressed("debug_4"):
-		_selected_component = CompSplitter;
-
-	if event.is_action_pressed("debug_5"):
-		_selected_component = CompLogicAnd;
-		
-	if event.is_action_pressed("debug_6"):
-		_selected_component = CompLogicOr;
 
 
-
-
-func _toggle_edit_mode():
-	_edit_mode = !_edit_mode;
-	if _edit_mode:
-		print("Edit mode activated");
-	else:
-		print("Edit mode deactivated");
+func _on_component_selected(component: Script) -> void:
+	_selected_component = component
---
+++ selector.gd
@@ -0,0 +1,43 @@
+extends CanvasLayer
+
+
+
+const CompConduit = preload("res://components/conduit.gd");
+const CompSource = preload("res://components/source.gd");
+const CompSink = preload("res://components/sink.gd");
+const CompSplitter = preload("res://components/splitter.gd");
+const CompLogicAnd = preload("res://components/logic_and.gd");
+const CompLogicOr = preload("res://components/logic_or.gd");
+
+
+
+signal component_selected(component : Script);
+
+
+
+func _on_interact_pressed() -> void:
+	component_selected.emit(null);
+
+
+func _on_conduit_pressed() -> void:
+	component_selected.emit(CompConduit);
+
+
+func _on_source_pressed() -> void:
+	component_selected.emit(CompSource);
+
+
+func _on_sink_pressed() -> void:
+	component_selected.emit(CompSink);
+
+
+func _on_splitter_pressed() -> void:
+	component_selected.emit(CompSplitter);
+
+
+func _on_and_pressed() -> void:
+	component_selected.emit(CompLogicAnd);
+
+
+func _on_or_pressed() -> void:
+	component_selected.emit(CompLogicOr);
